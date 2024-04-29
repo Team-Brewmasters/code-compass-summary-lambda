a53e76a03512ab5876c67e685f199e6a090d780d
John Aquino <johnaquino@outlook.com>
2024-04-29 12:07:53 -0400

    Fix event parameter in lambda_handler function

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from open_ai_service import call_chatgpt
 
 def lambda_handler(event, context):
     try:
-        github_url = event.get('githubURL')
+        github_url = event['queryStringParameters']['githubURL']
 
         file_content = get_repo_file_contents(github_url)
 
         open_ai_response = call_chatgpt(file_content)
         
-        print(open_ai_response)
         return {
             'statusCode': 200,
             'body': json.dumps(open_ai_response)
@@ -23,8 +22,8 @@ def lambda_handler(event, context):
             'body': str(e)
         }
     
-event = {
-    "githubURL": "https://www.github.com/Team-Brewmasters/code-compass-summary-lambda"
-}
+# event = {
+#     "githubURL": "https://www.github.com/Team-Brewmasters/code-compass-summary-lambda"
+# }
 
-lambda_handler(event, None)
+# lambda_handler(event, None)
