2961bdedbb563169c1fffb987f36945e935a7c16
John Aquino <johnaquino@outlook.com>
2024-04-29 11:00:32 -0400

    Add github_url parameter to lambda_handler function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def lambda_handler(event, context):
+    github_url = event.get('githubURL')
+    
     # Your code logic here
+    
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda!'
+        'body': str(github_url)
     }
